036af59efba575496b4f4749934a83ec8b050a87
langenoir <Ln1878@dhcp226.merusouth115.iit.edu>
2015-10-25 13:57:08 -0500

    added  iam into launch.sh

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --iam-instance-profile $7 --debug
