99a2767d85c018f316b049990c884e24ff4e220f
langenoir <Ln1878@dhcp117.merunorth106.iit.edu>
2015-10-28 14:58:58 -0500

    launch.sh modified

--- launch.sh
+++ launch.sh
@@ -18,16 +18,17 @@ aws ec2 wait instance-running --instance-ids ${instanceARR[@]}
 
 echo "instances are running."
 
-# 5. create ELB
+# 5. create ELB (*** need final modification ***)
+aws elb create-load-balancer --load-balancer-name $1 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 
+# 6. register instances with ELB (*** need final modification ***)
+aws elb register-instances-with-load-balancer --load-balancer-name $1 --instances ${instance_id[@]}
+# 7. ELB health-check configuration (*** need final modification ***)
+aws elb configure-health-check --load-balancer-name ...
 
-# 6. register instances with ELB
-
-# 7. ELB health-check configuration
-
-# 8. launch configuration
-
-# 9. auto-scaling , copy codes from BB, $$$$$ to be added
-
+# 8. launch configuration (*** need final modification ***)
 aws autoscaling create-launch-configuration 
+# 9. auto-scaling , codes from BB, $$$$$ to be added
+
+aws autoscaling create-launch-configuration //$$$$$$ here, to be added later after finished the overall lectures review
 
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-extended-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-name --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier
