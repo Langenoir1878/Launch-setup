960199bafa7a4155cb1b70c0c78bcf25432ecc9e
langenoir <Ln1878@dhcp84.merusouth116.iit.edu>
2015-10-28 23:40:21 -0500

    added create s3 bucket into launch shell

--- aws_Assignment_5_launch.sh
+++ aws_Assignment_5_launch.sh
@@ -12,3 +12,11 @@
 	
 
 aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --debug
+
+
+# creating s3 bucket and move images into the bucket for web display
+aws s3 mb s3://Student_yzln1878_AS5_S3_Bucket_Pls_Delete_Me_After_Testing
+# permission control of repo img
+chmod 755 hahaDino.png
+# move the image to newly created s3 bucket
+aws s3 cp hahaDino.png s3://Student_yzln1878_AS5_S3_Bucket_Pls_Delete_Me_After_Testing --acl public-read
