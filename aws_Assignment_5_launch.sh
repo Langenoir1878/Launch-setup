bcffc155960188144ea493e9e753777e5115cb59
langenoir <Ln1878@dhcp84.merusouth116.iit.edu>
2015-10-29 00:13:24 -0500

    added user file: aws_Assignment_5_install-env.sh

--- aws_Assignment_5_launch.sh
+++ aws_Assignment_5_launch.sh
@@ -10,8 +10,9 @@
 # cli syntax for fast arguements validations:
 # aws ec2 run-instances --image-id ami-d05e75b8 --count 1 --instance-type t2.micro --key-name KeyPair_2015OCT.pem --security-group-ids sg-c0f45da6 --subnet-id subnet-833692be --associate-public-ip-address
 	
+chmod 755 ../Environment-setup/aws-Assignment_5_install-env.sh
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --user-data file://../Environment-setup/aws-Assignment_5_install-env.sh --debug
 
 
 # creating s3 bucket and move images into the bucket for web display
