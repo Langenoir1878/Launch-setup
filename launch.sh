24f4e470ebbd0998b38c0f12f4d6986b900675ce
langenoir <Ln1878@dhcp46.merusouth117.iit.edu>
2015-11-04 11:04:46 -0600

    updated

--- launch.sh
+++ launch.sh
@@ -1,5 +1,17 @@
+# LAST UPDATED: NOV 4, 2015
 # Revised on OCT 25, 13:12:11 PM
 # Yiming Zhang
+# UPDATED INSTRUCTIONS UPON LAUNCHING NOV 4th, 2015
+# 10:09:35
+# 7 args to be put in the sequence below:
+
+# $1 image-ids
+# $2 count of instances to launch
+# $3 instance-type
+# $4 security-group-ids
+# $5 subnet-id
+# $6 key-name
+# $7 iam-instance-profile
 
 # 1. execute the cleanup.sh
 ./cleanup.sh
@@ -7,28 +19,65 @@
 # 2. declare an array in bash
 declare -a instanceARR
 
-# 3. mapfile???
-
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --iam-instance-profile $7 --debug
+# 3. mapfile (updated Nov 4, 2015)
+mapfile -t instanceARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --iam-instance-profile $7 --user-data file://../Environment-setup/install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g"--debug)
 
 echo ${instanceARR[@]}
 
-# 4. wait cli
+# 4. wait cli 
 aws ec2 wait instance-running --instance-ids ${instanceARR[@]}
 
 echo "instances are running."
 
-# 5. create ELB (*** need final modification ***)
-aws elb create-load-balancer --load-balancer-name $1 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 
-# 6. register instances with ELB (*** need final modification ***)
-aws elb register-instances-with-load-balancer --load-balancer-name $1 --instances ${instance_id[@]}
-# 7. ELB health-check configuration (*** need final modification ***)
-aws elb configure-health-check --load-balancer-name ...
+# 5. create ELB (updated 11/02/2015, Nov 4, 2015. dif *** ???security-group representations for ec2 and elb?)
+ELBURL = (`aws elb create-load-balancer --load-balancer-name SIMMON-THE-CAT --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --security-groups $4 --subnets $5 --output=text`)
+echo $ELBURL
 
-# 8. launch configuration (*** need final modification ***)
-aws autoscaling create-launch-configuration 
-# 9. auto-scaling , codes from BB, $$$$$ to be added
+#sleep
+echo -e "\nELB created, sleep 7 seconds"
+for i in {0..7}; do echo -ne '.'; sleep 1; done
+echo -e "\n"
 
-aws autoscaling create-launch-configuration //$$$$$$ here, to be added later after finished the overall lectures review
+# 6. register instances with ELB (updated 11/02/2015)
+aws elb register-instances-with-load-balancer --load-balancer-name SIMMON-THE-CAT --instances ${instanceARR[@]}
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-extended-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-name --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier
+# 7. ELB health-check configuration (updated Nov 4, 2015)
+aws elb configure-health-check --load-balancer-name SIMMON-THE-CAT --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+
+# wait additional 1 min
+echo -e "\nOpening the ELB in 1 minute in web browser"
+for i in {0..60}; do echo -ne '.'; sleep 1; done
+
+
+# 8. launch configuration (updated Nov 4, 2015)
+echo -e "\nCreating launch configuration: SIMMON-CONFIG-LAUNCH"
+aws autoscaling create-launch-configuration --launch-configuration-name SIMMON-CONFIG-LAUNCH --image-id $1 --instance-type $3 --security-groups $4 --key-name $6 --user-data file://../Environment-setup/install-webserver.sh --iam-instance-profile $7 --debug
+
+# 9. auto-scaling (updated Nov 4, 2015)
+echo -e "\nCreating the auto scaling group"
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name SIMMON-AUTO-SCALE --launch-configuration-name SIMMON-CONFIG-LAUNCH --load-balancer-name SIMMON-THE-CAT --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5
+
+# 10. rds instance 
+echo -e "\nCreating database"
+mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g")
+
+#if [ ${#dbInstanceARR[@]} -gt 0 ]
+#   then 
+#echo ${#dbInstanceARR[@]}
+
+LENGTH=${#dbInstanceARR[@]}
+#echo $LENGTH	
+
+for (( i=0; i<=${LENGTH}; i++));
+do
+   	if [[ ${dbInstanceARR[i]} == "SIMMON-THE-CAT-DB" ]]
+    then 
+    echo "SIMMON-THE-CAT-DB exists"
+    else
+    	# create the database instance, aws rds
+    	aws rds create-db-instance --db-instance-identifier SIMMON-THE-CAT-DB --db-instance-class db.t1.micro --engine MySQL --master-username LN1878 --master-user-password hesaysmeow --allocated-storage 5
+    fi  
+done
+
+
+
