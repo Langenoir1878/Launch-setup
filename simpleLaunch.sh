b4922b4c55ffb7ebf56d247ef9725591834f085e
langenoir <Ln1878@dhcp137.merusouth124.iit.edu>
2015-11-13 09:28:37 -0600

    simple test the db connection without everything else

---
+++ simpleLaunch.sh
@@ -0,0 +1,11 @@
+# testing simple launch without autoscaling
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6  --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://../Environment-setup/install-webserver.sh --debug
+
+#testing database connection
+aws rds create-db-instance --db-name simmoncatdb --db-instance-identifier SIMMON-THE-CAT-DB --db-instance-class db.t1.micro --engine mysql --master-username LN1878 --master-user-password hesaysmeow --allocated-storage 5
+
+#wait
+aws rds wait db-instance-available --db-instance-identifier SIMMON-THE-CAT-DB
+
+echo -e "\nPlease wait for a few minute, creating database : simmoncatdb . . ."
+
